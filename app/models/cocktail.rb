5fb99e4df81eec2275c46f841906361652d899db
Yeon Kim <awyeon@gmail.com>
2022-02-10 19:26:53 +0800

    added

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,2 +1,11 @@
 class Cocktail < ApplicationRecord
+  has_one_attached :photo
+
+  has_many :doses, dependent: :destroy
+  has_many :reviews, dependent: :destroy
+  has_many :ingredients, through: :doses
+
+  validates :name, presence: true
+  validates :photo, presence: true
+  validates :instruction, presence: true
 end
