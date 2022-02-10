7c72b3cf33e5ca4cfc4f4620368d7124101a76b1
Yeon Kim <awyeon@gmail.com>
2022-02-10 19:46:48 +0800

    added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'cocktails#index'
+
+  resources :cocktails do
+    resources :doses, only: %i[new create edit update]
+    resources :reviews, only: %i[new create edit update]
+  end
+
+  resources :cocktails, only: :destroy
+  resources :doses, only: :destroy
+  resources :reviews, only: :destroy
 end
