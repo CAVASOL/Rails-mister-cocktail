601c1773981de1744bf4b432d8ad8f32dc8a3fde
Yeon Kim <awyeon@gmail.com>
2022-02-10 19:59:10 +0800

    added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+require 'open-uri'
+require 'json'
+require 'yaml'
+
+puts 'Cleaning up database...'
+Cocktail.destroy_all
+puts 'Database cleaned'
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/filter.php?i=#{query}'
+JSON.parse(URI.parse(url).open.read)['drinks'].each do |ingredient|
+  new_ingredient = Ingredient.create(name: ingredient['strIngredient'])
+  new_ingredient.save!
+end
