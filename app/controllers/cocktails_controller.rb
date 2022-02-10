3f5921d30da205e506a8a428a93baf51b3ca0b66
Yeon Kim <awyeon@gmail.com>
2022-02-10 19:21:34 +0800

    added

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,44 @@
 class CocktailsController < ApplicationController
+  def index
+    if params[:query].present?
+      @query = params[:query]
+      @cocktails = Cocktail.where('name ILIKE ?', "%#{params[:query]}%")
+    else
+      @cocktails = Cocktail.all
+    end
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktails = Cocktail.all
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @cocktail.desctroy
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def cocktail_params
+    params.require('cocktail').permit(:name, :instruction, :photo)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
 end
