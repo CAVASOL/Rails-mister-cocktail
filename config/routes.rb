6bef38aeeea0f56088bfb1059cc6b0879ca85812
Yeon Kim <awyeon@gmail.com>
2022-02-14 16:55:46 +0800

    navbar testing

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,14 @@ Rails.application.routes.draw do
   root to: 'cocktails#index'
 
   resources :cocktails do
-    resources :doses, only: %i[new create edit update]
+    resources :ingredients, only: %i[create]
+    resources :doses, only: %i[create]
     resources :reviews, only: %i[new create edit update]
   end
 
   resources :cocktails, only: :destroy
   resources :doses, only: :destroy
   resources :reviews, only: :destroy
+
+  get 'explore', to: 'cocktails#explore', as: 'explore'
 end
