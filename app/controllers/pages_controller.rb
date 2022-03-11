673ead4f20689b7393d1475e23c5b8716223fd0e
Yeon Kim <awyeon@gmail.com>
2022-03-11 20:44:25 +0800

    added pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
+# app/controllers/pages_controller.rb
 class PagesController < ApplicationController
+  def mypage
+    @favourited = current_user.all_favorited
+  end
+
+  def profile
+    @user = current_user
+  end
 end
