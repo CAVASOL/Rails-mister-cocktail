fd65c53f5db048a85bbb98b0caef7e8968ea85b6
Yeon Kim <awyeon@gmail.com>
2022-03-11 19:48:06 +0800

    rails new

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
-# require 'open-uri'
-# app/controllers/movies_controller.rb
+# app/controllers/cocktails_controller.rb
 class CocktailsController < ApplicationController
   def index
     @ingredients = Ingredient.all
