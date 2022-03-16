196a3ea8c583970ee3fa2a2ef6504f30a8072641
Yeon Kim <awyeon@gmail.com>
2022-03-16 16:08:51 +0800

    testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   acts_as_favoritor
 
   has_many :cockatils
   validates :name, presence: true, length: { in: 2..26 }
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 end
