89b016313cb3318964e235b9d6784d190edaf2b5
Yeon Kim <awyeon@gmail.com>
2022-03-21 14:36:41 +0800

    customized simpleform

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -43,7 +43,6 @@ class CocktailsController < ApplicationController
   end
 
   def create
-    # @cocktails = Cocktail.all
     @cocktail = Cocktail.new(cocktail_params)
     @cockatil.user = current_user
     if @cocktail.save
