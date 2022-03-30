eac19a941de239c659a2d23d301f91c8f013b085
Yeon Kim <awyeon@gmail.com>
2022-03-30 15:00:18 +0800

    testing sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'cocktails#index'
+  devise_for :users do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
 
+  root to: 'cocktails#index'
   resources :cocktails do
     resources :ingredients, only: %i[new create]
     resources :doses, only: %i[new create]
@@ -15,7 +16,6 @@ Rails.application.routes.draw do
   resources :ingredients, only: %i[edit update destroy]
   resources :reviews, only: :destroy
 
-  get '/users/:id', to: 'users#show', as: 'users'
   get 'profile', to: 'users#profile', as: 'profile'
   get 'explore', to: 'cocktails#explore', as: 'explore'
   get 'mypage', to: 'cocktails#mypage', as: 'mypage'
