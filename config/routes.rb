56aabb49ee33625f46b653c4d725b2a72e8a1826
Yeon Kim <awyeon@gmail.com>
2022-03-31 20:41:41 +0800

    jquery error fixed

--- config/routes.rb
+++ config/routes.rb
@@ -8,19 +8,17 @@ Rails.application.routes.draw do
     resources :ingredients, only: %i[new create]
     resources :doses, only: %i[new create]
     resources :reviews, only: %i[new create]
-    resources :favorites, only: %i[create destroy]
+    resources :favorites, only: :create
+    delete 'favorites', to: 'favorites#destroy'
   end
 
   resources :doses, only: :destroy
   resources :ingredients, only: %i[edit update destroy]
   resources :reviews, only: :destroy
-  # resources :favorites, only: %i[create destroy]
 
   get 'profile', to: 'users#profile', as: 'profile'
   get 'explore', to: 'cocktails#explore', as: 'explore'
   get 'mypage', to: 'cocktails#mypage', as: 'mypage'
   get 'index', to: 'ingredients#index', as: 'ingredient_index'
   get 'new', to: 'ingredients#new', as: 'ingredient_new'
-  # get '/cocktails/:cocktail_id/favorites', to: 'favorites#create'
-  # get '/favorites', to: 'favorites#destroy'
 end
