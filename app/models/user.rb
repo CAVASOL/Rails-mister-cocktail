e482cd6fa68c2ebbc6b35388d73d0374c77ea283
Yeon Kim <awyeon@gmail.com>
2022-03-31 15:13:27 +0800

    testing routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :cockatils, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_many :ingredients, dependent: :destroy
+  # has_many :favorites, dependent: :destroy
 
   validates :name, presence: true, length: { in: 2..26 }
 
