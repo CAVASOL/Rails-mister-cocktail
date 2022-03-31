407e82ea6128637aed3688fa3427fa5d644e5014
Yeon Kim <awyeon@gmail.com>
2022-03-31 14:55:35 +0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,19 +8,19 @@ Rails.application.routes.draw do
     resources :ingredients, only: %i[new create]
     resources :doses, only: %i[new create]
     resources :reviews, only: %i[new create]
-    resources :favorites, only: %i[create destroy] do
-      get 'favorites', to: 'favorites#create'
-      delete 'favorites', to: 'favorites#destroy'
-    end
+    resources :favorites, only: %i[create destroy]
   end
 
   resources :doses, only: :destroy
   resources :ingredients, only: %i[edit update destroy]
   resources :reviews, only: :destroy
+  # resources :favorites, only: %i[create destroy]
 
   get 'profile', to: 'users#profile', as: 'profile'
   get 'explore', to: 'cocktails#explore', as: 'explore'
   get 'mypage', to: 'cocktails#mypage', as: 'mypage'
   get 'index', to: 'ingredients#index', as: 'ingredient_index'
   get 'new', to: 'ingredients#new', as: 'ingredient_new'
+  # get '/cocktails/:cocktail_id/favorites', to: 'favorites#create'
+  # get '/favorites', to: 'favorites#destroy'
 end
